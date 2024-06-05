3f5d04355fdefd80f73905613f68be8d2e54267d
MagoLione <76831281+MagoLione@users.noreply.github.com>
2024-06-05 19:20:23 +0200

    Delete python directory

--- python/scripts/CosmicRay.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-# A class to save a CosmicRay with a date, an angle, and a description.
-class CosmicRay:
-    def __init__(self, date: datetime, angle: int, description: str):
-        self.date: datetime = date
-        self.angle: int = angle
-        self.description: str = description
