79d1f49e916b104d4a7ad8e56411a2b90f6375cb
BuildTools <simonefurby@outlook.com>
2024-06-05 19:19:12 +0200

    Adding python scripts.

---
+++ python/scripts/local_reader.py
@@ -0,0 +1,132 @@
+import os
+import json
+import time
+from datetime import datetime
+
+from scripts.logs import log, Level
+
+from scripts.Track import Settings
+from scripts.local_writer import folder, settings_folder, automode_file, settings_file
+from scripts.local_writer import initialize_settings, reinitialize_active, reinitialize_angle
+from scripts.dates import get_todays_file_name
+
+# Search if today's file exists in local folder using date.
+def is_there_the_correct_file(date: datetime) -> bool:
+    day_name = get_todays_file_name(date) # Getting file name.
+        
+    return is_there_the_correct_file_by_name(day_name)
+
+# Search if today's file exists in local folder using name.
+def is_there_the_correct_file_by_name(day_name: str) -> bool:
+    
+    for file in os.listdir(folder): # For each file in local folder.
+        if file == f"{day_name}.csv": # If is equal to the file it's seaching for.
+            return True
+        
+    return False
+
+# Reads the settings file, returns a Settings object.
+def read_settings() -> Settings:
+    a_data = None
+    data = None
+    
+    a_i = 0
+    
+    while a_data == None:
+        
+        try:
+            with open(os.path.join(settings_folder, automode_file), "r") as a_f: # Opening file in reading mode.
+                json_a_data = a_f.read() # Reading file.
+                
+                if json_a_data == "": # If the is empty.
+                    return initialize_settings() # Initializing settings.
+                    
+                a_data = json.loads(json_a_data) # Deconding json data.
+                
+            break # Exiting the while cycle.
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            if a_i > 0:
+                log(
+                    e=f"""
+                    An error occurred reading automode.json file:
+                            
+                    If it only occurred once it isn't a problem.
+                    If it repeats check the file status and the users.
+                    
+                    {e}
+                    
+                    """,
+                    flag="localreader",
+                    level=Level.L
+                )
+        
+        a_i += 1
+        
+        time.sleep(0.2)
+            
+    is_enabled = a_data.get('enabled') # Getting enabled setting value.
+        
+    if not is_enabled: # If is None.
+        return initialize_settings() # Initializing settings.
+    
+    
+    i = 0
+    
+    while data == None:
+        
+        try:
+            with open(os.path.join(settings_folder, settings_file), "r") as f: # Opening file in reading mode.
+                json_data = f.read() # Reading file.
+                
+                if json_data == "": # If the is empty.
+                    return initialize_settings() # Initializing settings.
+                
+                data = json.loads(json_data) # Deconding json data.
+                
+            break # Exiting the while cycle.
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            if i > 0:
+                log(
+                    e=f"""
+                    An error occurred reading settings.json file:
+                            
+                    If it only occurred once it isn't a problem.
+                    If it repeats check the file status and the users.
+                    
+                    {e}
+                    
+                    """,
+                    flag="localreader",
+                    level=Level.L
+                )
+        
+        i += 1
+        
+        time.sleep(0.2)
+        
+        
+    
+    active = data.get('active') # Getting active setting value.
+    angle = data.get('angle') # Getting angle setting value.
+
+    if active == None or angle == None: # If a setting is None.
+        settings = initialize_settings() # Initializing and saving settings.
+    else:
+        # Saving settings with readed one.
+        settings = Settings(
+            active = active,
+            angle = angle,
+            description = data.get('description')
+        )
+        
+        # Handling active invalid values.
+        if not isinstance(active, bool):
+            print("\nInvalid value in Active field.")
+            settings = reinitialize_active(settings)
+        
+        # Handling angle invalid values.
+        if not isinstance(angle, int) or angle < 0 or angle > 180:
+            print("\nInvalid value in Angle field.")
+            settings = reinitialize_angle(settings)
+        
+    return settings
---
+++ python/scripts/local_writer.py
@@ -0,0 +1,195 @@
+import os
+import time
+import json
+
+from datetime import datetime
+
+from CosmicRay import CosmicRay
+from scripts.logs import log, Level
+from scripts.dates import get_todays_file_name, from_date_to_string
+from scripts.Track import Settings
+
+folder = "YOUR-DATA-FOLDER"
+
+settings_folder = "YOUR-SETTINGS-FOLDER"
+
+automode_file = "automode.json"
+settings_file = "settings.json"
+
+error_flag = "localwriter"
+
+index1 = 0
+index2 = 0
+
+# Deletes useless files in local data folder, like old CSVs.
+def delete_useless_files(date: datetime):
+    
+    day_name = get_todays_file_name(date) # Getting name of the only usefull file.
+    
+    for file in os.listdir(folder): # For each file in local folder.
+        if file != f"{day_name}.csv": # If is named different.
+            os.remove(os.path.join(folder, file)) # Removing file.
+            
+# Append CosmicRay's data to the csv.
+def write_csv(cr: CosmicRay):
+    global index1
+    global index2
+    
+    index1 += 1
+        
+    file = f"{get_todays_file_name(cr.date)}.csv" # Getting file name.
+    file_path = os.path.join(folder, file) # Getting file path.
+    
+    formatted_date = from_date_to_string(cr.date, "D") # Getting fully formatted date.
+    
+    with open(file_path, "a") as f: # Opening file in append mode.
+        f.write(f"{formatted_date},{cr.angle},{cr.description}\n") # Writing the file.
+        
+    index2 += 1
+    
+    if index1 > index2:        
+        log(
+            e=f"""
+            An error occurred while writing measurements to the file:
+            
+            Some measurements were lost.
+            If it repeats, check if other programs are using the file.
+            
+            Total number of missed measurements' estimate: {index1-index2}
+            
+            """,
+            flag=error_flag,
+            level=Level.H
+        )
+    elif index1 < index2:
+        log(
+            e=f"""
+            An error occurred while writing measurements to the file:
+            
+            Some measurements were write multiple times.
+            It hasn't explanation.
+            
+            Total number of multipled measurements' estimate: {index2-index1}
+            
+            """,
+            flag=error_flag,
+            level=Level.WTF
+        )
+
+# Initialize settings files.
+def initialize_settings() -> Settings:
+    print("Initializing settings...")
+    
+    enabled = True
+        
+    while True:
+        try:
+            with open(os.path.join(settings_folder, automode_file), "w") as a_f: # Opening automode file in writing mode.
+        
+                a_data = {'enabled': enabled}
+                
+                a_f.write(json.dumps(a_data, indent=4)) # Writing data in json format.
+                
+            break
+        except (OSError) as e:
+            log(
+                e=f"""
+                An error occurred initializing automode.json file:
+                
+                Check file's users.
+                
+                {e}
+                """,
+                flag=error_flag,
+                level=Level.M
+            )
+            
+            time.sleep(0.2)
+    
+    active = True
+    angle = 0
+    description = None
+        
+    while True:
+        try:
+            with open(os.path.join(settings_folder, settings_file), "w") as f: # Opening settings file in writing mode.
+        
+                data = {'active': active, 'angle': angle, 'description': description}
+                
+                f.write(json.dumps(data, indent=4)) # Writing data in json format.
+                
+            break
+        except (OSError) as e:
+            log(
+                e=f"""
+                An error occurred initializing settings.json file:
+                
+                Check file's users.
+                
+                {e}
+                """,
+                flag=error_flag,
+                level=Level.M
+            )
+            
+            time.sleep(0.2)
+    
+        
+    print("Done.")
+        
+    return Settings(active, angle, description) # Returning a Settings object.
+
+# Resets settings.
+def reset_settings():
+    print("Resetting settings...")
+    
+    enabled = False
+    
+    with open(os.path.join(settings_folder, automode_file), "w") as a_f: # Opening automode file in writing mode.
+        
+        a_data = {'enabled': enabled}
+        
+        a_f.write(json.dumps(a_data, indent=4)) # Writing data in json format.
+    
+    active = None
+    angle = None
+    description = None
+    
+    with open(os.path.join(settings_folder, settings_file), "w") as f: # Opening settings file in writing mode.
+        
+        data = {'active': active, 'angle': angle, 'description': description}
+        
+        f.write(json.dumps(data, indent=4)) # Writing data in json format.
+        
+    print("Done.")
+    
+# Reinitialize the active setting.
+def reinitialize_active(settings: Settings) -> Settings:
+    print("Reinitializing active...")
+    
+    with open(os.path.join(settings_folder, settings_file), "w") as f: # Opening file in writing mode.
+        
+        data = {'active': True, 'angle': settings.angle, 'description': settings.description}
+        
+        f.write(json.dumps(data, indent=4)) # Writing data in json format.
+        
+    settings.angle = 0
+    
+    print("Active is set to True.")
+    
+    return settings
+    
+# Reinitialize the angle setting.
+def reinitialize_angle(settings: Settings) -> Settings:
+    print("Reinitializing angle...")
+    with open(os.path.join(settings_folder, settings_file), "w") as f: # Opening file in writing mode.
+        
+        data = {'active': settings.active, 'angle': 0, 'description': settings.description}
+        
+        f.write(json.dumps(data, indent=4)) # Writing data in json format.
+        
+    settings.angle = 0
+    
+    print("Angle is set to 0.")
+    
+    return settings
